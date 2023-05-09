4a4e58cddd4fcad4d4be7decd09873b50be16d58
Adi Widyawan <day4die@gmail.com>
2023-05-09 10:44:15 +0700

    Jawaban soal No 1.

---
+++ soal-1/script.js
@@ -0,0 +1,14 @@
+function cetakTelurAtauAyam(x, y) {
+  var x_output = x;
+  for(var i = 0; i < (y - 1); i++) {
+    x_output = x + 2 * i;
+    console.log("Iterasi %d", i + 1);
+    console.log("Output x = %d", x_output);
+  }
+
+  const text = x_output % 2 == 0 ? "AYAM" : "TELUR";
+
+  console.log(text);
+}
+
+cetakTelurAtauAyam(1, 6);
