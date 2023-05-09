6ff36166e1f2fa1c4af1090d52ed17ec2dc3d71f
Adi Widyawan <day4die@gmail.com>
2023-05-09 12:41:41 +0700

    Update script agar bisa di-eksekusi menggunakan NodeJS via CLI.

--- soal-1/script.js
+++ soal-1/script.js
@@ -1,14 +1,19 @@
+#!/usr/bin/env node
+
 function cetakTelurAtauAyam(x, y) {
   var x_output = x;
   for(var i = 0; i < (y - 1); i++) {
     x_output = x + 2 * i;
     console.log("Iterasi %d", i + 1);
-    console.log("Output x = %d", x_output);
+    console.log("   Output x = %d", x_output);
   }
 
   const text = x_output % 2 == 0 ? "AYAM" : "TELUR";
 
-  console.log(text);
+  console.log("Teks = %s", text);
 }
 
-cetakTelurAtauAyam(0, 3);
+var x = parseInt(process.argv[2]);
+var y = parseInt(process.argv[3]);
+console.log("x=%d, y=%d", x, y);
+cetakTelurAtauAyam(x, y);
